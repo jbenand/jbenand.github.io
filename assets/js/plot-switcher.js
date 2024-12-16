8da6f8c89753b4f17b383a49c2c201ad1cd6b53f
shrinidhivelan <shrinidhi.velan@epfl.ch>
2024-12-16 18:10:54 +0100

    try

--- assets/js/plot-switcher.js
+++ assets/js/plot-switcher.js
@@ -9,9 +9,11 @@ const plot2Path = 'assets/plots/feminist_movies_interactive_gpt_proportion.html'
 
 // Add event listeners for buttons
 button1.addEventListener('click', () => {
-  plotFrame.src = plot1Path; // Change the iframe source to actors_age.html
+  console.log('Button 1 clicked');
+  plotFrame.src = plot1Path; // Change the iframe source to plot1
 });
 
 button2.addEventListener('click', () => {
-  plotFrame.src = plot2Path; // Change the iframe source to actors_age2.html
+  console.log('Button 2 clicked');
+  plotFrame.src = plot2Path; // Change the iframe source to plot2
 });
